b5dcf7e3215502e9942e5e91296cb1043ed06ece
Vitaly Miroshkin <sysop14@gmail.com>
2021-05-15 00:20:25 +0300

    Add validates and relation User to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, :datetime, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, :email, presence: true
+  validates :name, length: { maximum: 40 }
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
