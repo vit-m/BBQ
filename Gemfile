49de55b1ab9ea65e7526f2bd73b0d35e1fccb2ab
Vitaly Miroshkin <sysop14@gmail.com>
2021-05-15 23:01:59 +0300

    Add gem devise

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.3'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
+gem 'devise'
 gem 'webpacker', '~> 5.0'
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   has_many :events
 
-  validates :name, :email, presence: true
   validates :name, length: { maximum: 40 }
-  validates :email, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Пользователь №#{rand(999)}" if self.name.blank?
+  end
 end
