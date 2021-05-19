26c1eca9f9dac3aa77a716b688cae115328a88bb
Vitaly Miroshkin <sysop14@gmail.com>
2021-05-19 21:49:45 +0300

    Fix flash messages

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,5 +1,6 @@
 import Rails from "@rails/ujs"
 import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/js/src/alert';
 import '../styles/application'
 
 const images = require.context('../images/', true)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message
+            end)
+    end
+    nil
+  end
 end
