7405267ad6fffe98a20050173dae5ab3b5e63b3c
Vitaly Miroshkin <sysop14@gmail.com>
2021-05-23 21:43:18 +0300

    Add Air Datepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,8 +1,14 @@
 import Rails from "@rails/ujs"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/js/src/alert';
+import 'air-datepicker/dist/js/datepicker'
 import '../styles/application'
 
 const images = require.context('../images/', true)
+
+;(function (window, $, undefined) { ;(function () {
+      defaults = {
+          timepicker: true
+      } } )})
 
 Rails.start()
