7c71759380a43469d39468bfe6cc1747a355beac
Vitaly Miroshkin <sysop14@gmail.com>
2021-06-14 22:58:14 +0300

    Add Pin-Code for Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :password_guard!, only: [:show]
 
   # GET /events
   def index
@@ -51,15 +52,30 @@ class EventsController < ApplicationController
   end
 
   private
-    def set_current_user_event
-      @event = current_user.events.find(params[:id])
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
     end
 
-    def set_event
-      @event = Event.find(params[:id])
+    unless @event.pincode_valid?(cookies.permanent["events_#{@event.id}_pincode"])
+      flash.now[:alert] = t('.wrong_pincode') if params[:pincode].present?
+      # flash_messages[:alert] = t('.wrong_pincode') if params[:pincode].present?
+      render 'password_form'
     end
+  end
 
-    def event_params
-      params.require(:event).permit(:title, :description, :address, :datetime)
-    end
+  def set_current_user_event
+    @event = current_user.events.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :address, :datetime, :pincode)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_102931) do
+ActiveRecord::Schema.define(version: 2021_06_13_194152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_102931) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
