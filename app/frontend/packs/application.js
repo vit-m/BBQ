ebf8fcb08148b3ea64dc06ec3b97633d25e9b951
Vitaly Miroshkin <sysop14@gmail.com>
2021-06-15 14:07:59 +0300

    Add lightbox2 for images

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/js/src/alert';
 import 'air-datepicker/dist/js/datepicker.min'
+import 'lightbox2/dist/js/lightbox.min'
 import '../styles/application'
 
 const images = require.context('../images/', true)
